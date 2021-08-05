430a7f8523bb0565ab9a53ca70e2b09725cc4d50
akukamil <chamilioner_kid@mail.ru>
2021-08-05 15:48:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1145,7 +1145,7 @@ var fp_menu= {
 			let ls=data.response[0].last_seen;
 						
 			objects.fp_name_text.text=first_name+" "+last_name;
-			objects.fp_last_seen_text.text=new Date(ls.time).toLocaleString();
+			objects.fp_last_seen_text.text=new Date(ls.time*1000).toLocaleString();
 			//загружаем аватар игрока
 			var loaderOptions = {loadType: PIXI.loaders.Resource.LOAD_TYPE.IMAGE};
 			var loader = new PIXI.Loader();
