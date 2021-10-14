043b336017dd72e22ded79555c2b9727dad24d95
akukamil <chamilioner_kid@mail.ru>
2021-10-14 21:01:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2604,7 +2604,7 @@ var auth = function() {
 
 			local: function(repeat = 0) {
 
-				game_platform="LOCAL";
+				game_platform="YANDEX";
 
 				//ищем в локальном хранилище
 				let local_uid = localStorage.getItem('uid');
